dcd2a46b60f27de900c868627f9bcad62079356a
Shekhawat2 <anandsingh215@yahoo.com>
2018-01-12 00:19:11 +0530

    Statusbar notification ticker [2/2]
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- src/com/nitrogen/settings/fragments/NotificationSettings.java
+++ src/com/nitrogen/settings/fragments/NotificationSettings.java
@@ -4,7 +4,9 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
+import android.content.ContentResolver;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
@@ -25,6 +27,7 @@ public class NotificationSettings extends SettingsPreferenceFragment
 
     private ListPreference mAnnoyingNotifications;
     private Preference mChargingLeds;
+    private ListPreference mTickerMode;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -33,6 +36,8 @@ public class NotificationSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.nitrogen_settings_notifications);
 
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
         if (!Utils.isVoiceCapable(getActivity())) {
@@ -56,6 +61,14 @@ public class NotificationSettings extends SettingsPreferenceFragment
         }
         mAnnoyingNotifications.setOnPreferenceChangeListener(this);
 
+        mTickerMode = (ListPreference) findPreference("ticker_mode");
+        mTickerMode.setOnPreferenceChangeListener(this);
+        int tickerMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                1, UserHandle.USER_CURRENT);
+        mTickerMode.setValue(String.valueOf(tickerMode));
+        mTickerMode.setSummary(mTickerMode.getEntry());
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -70,6 +83,16 @@ public class NotificationSettings extends SettingsPreferenceFragment
                     .setSummary(mAnnoyingNotifications.getEntries()[notificationThresholdIndex]);
             return true;
         }
+
+        else if (preference == mTickerMode) {
+            int tickerMode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER, tickerMode, UserHandle.USER_CURRENT);
+            int index = mTickerMode.findIndexOfValue((String) newValue);
+            mTickerMode.setSummary(
+                    mTickerMode.getEntries()[index]);
+            return true;
+	}
 
         return false;
     }
