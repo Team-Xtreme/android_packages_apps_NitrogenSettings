ResurrectionRemix/Resurrection_packages_apps_Settings

Force Expanded Notifications & Disable Immersive Messages [2/2]
Change-Id: I502b09b76405311a7e46c1949e9301b31c1b9261
Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/android/settings/rr/NotificationSettings.java
@@ -1,0 +1,83 @@
+/*Copyright (C) 2015 The ResurrectionRemix Project
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+package com.android.settings.rr;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.provider.Settings;
+
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.rr.Preferences.SystemSettingSwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class NotificationSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+    private SwitchPreference mDisableIM;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.rr_notif_settings);
+        final Activity activity = getActivity(); 
+		ContentResolver resolver = getActivity().getContentResolver();
+
+        mDisableIM = (SwitchPreference) findPreference(DISABLE_IMMERSIVE_MESSAGE);
+        mDisableIM.setOnPreferenceChangeListener(this);
+        int DisableIM = Settings.System.getInt(getContentResolver(),
+                DISABLE_IMMERSIVE_MESSAGE, 0);
+        mDisableIM.setChecked(DisableIM != 0);
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mDisableIM) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), DISABLE_IMMERSIVE_MESSAGE,
+                    value ? 1 : 0);
+            return true;
+        } 
+        return false;
+	}
+
+
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.RESURRECTED;
+    }
+
+}

Team-Xtreme/android_packages_apps_NitrogenSettings

src/com/nitrogen/settings/fragments/NotificationSettings.java
@@ -1,81 +1,83 @@
+/*Copyright (C) 2015 The ResurrectionRemix Project
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
 package com.nitrogen.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
+import android.app.Activity;
+import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.provider.Settings;
+
 
 import com.android.settings.R;
-
 import com.android.settings.SettingsPreferenceFragment;
+import com.nitrogen.settings.preferences.SystemSettingSwitchPreference;
 
-import com.nitrogen.settings.preferences.Utils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-public class NotificationSettings extends SettingsPreferenceFragment
-                        implements OnPreferenceChangeListener {
+public class NotificationSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
-    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
-    private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+    private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+    private SwitchPreference mDisableIM;
 
-    private ListPreference mAnnoyingNotifications;
-    private Preference mChargingLeds;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.rr_notif_settings);
+        final Activity activity = getActivity(); 
+		ContentResolver resolver = getActivity().getContentResolver();
 
-        addPreferencesFromResource(R.xml.nitrogen_settings_notifications);
-
-        PreferenceScreen prefScreen = getPreferenceScreen();
-
-        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
-        if (!Utils.isVoiceCapable(getActivity())) {
-            prefScreen.removePreference(incallVibCategory);
-        }
-
-        mChargingLeds = (Preference) findPreference("charging_light");
-        if (mChargingLeds != null
-                && !getResources().getBoolean(
-                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
-            prefScreen.removePreference(mChargingLeds);
-        }
-
-        mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
-        int notificationThreshold = Settings.System.getInt(getContentResolver(),
-                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
-        mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
-        int valueIndex = mAnnoyingNotifications.findIndexOfValue(String.valueOf(notificationThreshold));
-        if (valueIndex > 0) {
-            mAnnoyingNotifications.setSummary(mAnnoyingNotifications.getEntries()[valueIndex]);
-        }
-        mAnnoyingNotifications.setOnPreferenceChangeListener(this);
-
+        mDisableIM = (SwitchPreference) findPreference(DISABLE_IMMERSIVE_MESSAGE);
+        mDisableIM.setOnPreferenceChangeListener(this);
+        int DisableIM = Settings.System.getInt(getContentResolver(),
+                DISABLE_IMMERSIVE_MESSAGE, 0);
+        mDisableIM.setChecked(DisableIM != 0);
     }
 
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mAnnoyingNotifications) {
-            String notificationThreshold = (String) newValue;
-            int notificationThresholdValue = Integer.parseInt(notificationThreshold);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, notificationThresholdValue);
-            int notificationThresholdIndex = mAnnoyingNotifications
-                    .findIndexOfValue(notificationThreshold);
-            mAnnoyingNotifications
-                    .setSummary(mAnnoyingNotifications.getEntries()[notificationThresholdIndex]);
+        if (preference == mDisableIM) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), DISABLE_IMMERSIVE_MESSAGE,
+                    value ? 1 : 0);
             return true;
-        }
-
+        } 
         return false;
-    }
+	}
+
+
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.NITROGEN_SETTINGS;
+        return MetricsEvent.NITROGEN_SETTINGS;
     }
+
 }